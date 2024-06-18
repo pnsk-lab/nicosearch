f07226652e3b430ac2e5d5b56af2edd43d904c24
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-06-18 13:41:32 +0000

    smart

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
 import type { Component } from 'solid-js'
-import { createSignal, createEffect, Show, For } from 'solid-js'
-import DB from './DB.json'
-
-type video = {
-  id: string
-  title: string
-  registeredAt: string
-  thumbnail: {
-    url: string
-  }
-}
+import { createSignal, createEffect, Show, For, onMount, createMemo } from 'solid-js'
+import type { Video } from './types'
+import Fuse, { type FuseResult } from 'fuse.js'
 
 const App: Component = () => {
-  const [ keys, setKeys ] = createSignal<string[]>([])
-  const [ results, setResults ] = createSignal<video[]>([])
-  const [ startIndex, setStartIndex ] = createSignal<number>(0)
-  createEffect(() => {
-    const regex = new RegExp('^' + keys().map((key:string) => `(?=.*${key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`).join(''))
-    //@ts-ignore
-    setResults(Object.values(DB).filter((video: video) => regex.test(video.title)))
+  const [query, setQuery] = createSignal<string>('')
+  const [videos, setVideos] = createSignal<Video[]>()
+  const [fuse, setFuse] = createSignal<Fuse<Video>>()
+
+  onMount(async () => {
+    const res = await fetch('/DB.json').then(res => res.json())
+    const videos = Object.values(res) as Video[]
+    setVideos(videos)
+    setFuse(new Fuse(videos, {
+      keys: ['title', 'description']
+    }))
   })
+
+  const result = createMemo(() => {
+    const gotFuse = fuse()
+    if (!gotFuse) {
+      return null
+    }
+    return gotFuse.search(query())
+  })
+
   return (
     <div class="px-10 sm:px-20 mb-20">
       <h1 class="mt-20 w-full text-center text-3xl font-cutiveMono">NICOSEARCH</h1>
-      <input onChange={(e) => { setKeys(e.target.value.replace('　',' ').split(' ')); setStartIndex(0); }} placeholder='And検索はスペース区切りで！' class='mt-5 border border-gray-400 border-1 w-full h-14 px-5 rounded-full'/>
-      <Show when={keys().length && keys()[0] != ""}>
-        <For each={results().slice(startIndex(), startIndex()+10 )}>{(result) => 
-          <Result video={result}/>
-        }</For>
-        <p class="w-full text-center mt-5 text-lg">
-          <For each={Array.from({ length: Math.floor(results().length / 10) + 1 }, (_, i) => i * 10)}>{(indexNum) => 
-            <>
-              <Show when={indexNum != startIndex()} fallback={<span class="text-gray-500 mr-2">{(indexNum/10) + 1}</span>}>
-                <span class="text-blue-500 underline mr-2" onClick={() => setStartIndex(indexNum)}>{(indexNum/10) + 1}</span>
-              </Show>
-              <Show when={(indexNum + 10) % 100 == 0}><br /></Show>
-            </>
-          }</For>
-        </p>
+      <input value={query()} onInput={(e) => {
+        setQuery(e.target.value)
+      }} placeholder='' class='mt-5 border border-gray-400 border-1 w-full h-14 px-5 rounded-full'/>
+      <Show when={videos} fallback={<div class="text-center text-2xl p-4">Downloading Database...</div>}>
+        <Show when={result()?.length !== 0} fallback={<div class="text-center text-2xl p-4">結果がありません...</div>}>
+          <For each={result()}>{(v) => <VideoPreview video={v} />}</For>
+        </Show>
       </Show>
       <div class="text-center text-gray-500 mt-10">
         <hr class="h-0.5 my-3" />
@@ -58,17 +55,17 @@ const App: Component = () => {
   )
 }
 
-const Result: Component<{ video: video }> = (props) => {
+const VideoPreview: Component<{ video: FuseResult<Video> }> = (props) => {
   return (
-    <a href={`https://www.nicovideo.jp/watch_tmp/${props.video.id}`}>
+    <a href={`https://www.nicovideo.jp/watch_tmp/${props.video.item.id}`} target="_blank" rel="noreferrer">
       <div class="mt-5 w-full grid grid-cols-4 gap-2">
         <div class="grid col-span-2 sm:col-span-1">
-          <img src={props.video.thumbnail.url} alt="thumbnail" class="m-auto" loading="lazy"/>
+          <img src={props.video.item.thumbnail.url} alt="thumbnail" class="m-auto" loading="lazy"/>
         </div>
         <div class="grid col-span-2 sm:col-span-3">
-          <p class="font-bold text-base sm:text-lg">{props.video.title}</p>
+          <p class="font-bold text-base sm:text-lg">{props.video.item.title}</p>
           <p class="text-gray-500 text-sm sm:text-base">
-            {new Date(props.video.registeredAt).toLocaleDateString('ja-JP')}
+            {new Date(props.video.item.registeredAt).toLocaleDateString('ja-JP')}
           </p>
         </div>
       </div>
---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export interface Video {
+  type: 'essential'
+  id: string
+  title: string
+  registeredAt: string
+  count: { view: number; comment: number; mylist: number; like: number }
+  thumbnail: {
+    url: string
+    middleUrl: null
+    largeUrl: null
+    listingUrl: string
+    nHdUrl: string
+  }
+  duration: number
+  shortDescription: string
+  latestCommentSummary: string
+  isChannelVideo: false
+  isPaymentRequired: false
+  playbackPosition: null
+  owner: {
+    ownerType: 'hidden'
+    type: 'unknown'
+    visibility: 'hidden'
+    id: null
+    name: null
+    iconUrl: null
+  }
+  requireSensitiveMasking: false
+  videoLive: null
+  isMuted: false
+  '9d091f87': true
+  acf68865: false
+}
