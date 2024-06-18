f74811f168303e9421c33b09cc3c3a8db2910db9
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-06-18 13:43:29 +0000

    remove eruda

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import App from './App'
 import './index.css'
 
 const root = document.getElementById('root')
-;(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
-
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
