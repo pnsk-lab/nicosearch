2d458dbbbc73824edd724eddaead503748509b54
kombumori <76637001+kombumori@users.noreply.github.com>
2024-06-20 16:39:10 +0900

    add: <Show when={query() !== ""}>

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,10 @@ const App: Component = () => {
         setQuery(e.target.value)
       }} placeholder='' class='mt-5 border border-gray-400 border-1 w-full h-14 px-5 rounded-full'/>
       <Show when={true} fallback={<div class="text-center text-2xl p-4">Downloading Database...</div>}>
-        <Show when={result()?.length !== 0} fallback={<div class="text-center text-2xl p-4">結果がありません...</div>}>
-          <For each={result()}>{(v) => <VideoPreview video={v} />}</For>
+        <Show when={query() !== ''}>
+          <Show when={result()?.length !== 0} fallback={<div class="text-center text-2xl p-4">結果がありません...</div>}>
+            <For each={result()}>{(v) => <VideoPreview video={v} />}</For>
+          </Show>
         </Show>
       </Show>
       <div class="text-center text-gray-500 mt-10">
